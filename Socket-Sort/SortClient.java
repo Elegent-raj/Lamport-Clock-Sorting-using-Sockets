a2b3426d1763e320c52adda291bf8d83ba70c809
Rohit <raj85404@gmail.com>
2021-10-11 23:45:34 +0530

    Final Sorting Code

--- Socket-Sort/SortClient.java
+++ Socket-Sort/SortClient.java
@@ -1,39 +1,38 @@
+
 // Rohit Raj Anirudh Veludandi
 // Client side code for sorting
 // TCP/IP implementation
+// Run SortClient only after running SortServer
 import java.io.*;
 import java.net.*;
-class SortClient
-{
-	public static void main(String ar[]) throws Exception
-	{
+
+class SortClient {
+	public static void main(String ar[]) throws Exception {
 		// Creating socket and connecting it to server
-		Socket s=new Socket("localhost",12345); 
+		Socket s = new Socket("localhost", 12345);
 		// Serializing the input and output streams
-		PrintWriter p=new PrintWriter(s.getOutputStream());
-		BufferedReader in=new BufferedReader(new InputStreamReader(s.getInputStream()));
-		BufferedReader ink=new BufferedReader(new InputStreamReader(System.in));
+		PrintWriter p = new PrintWriter(s.getOutputStream());
+		BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+		BufferedReader ink = new BufferedReader(new InputStreamReader(System.in));
 		// Taking the input from client
 		System.out.println("How many numbers to sort? ");
-		int num=Integer.parseInt(ink.readLine());
+		int num = Integer.parseInt(ink.readLine());
 		p.println(num);
 		p.flush();
-		System.out.println("Enter "+num+" numbers to sort :");
-		String sarr[]=new String[num];
-		for(int i=0;i<num;i++)
-		{
-			System.out.print("No. "+(i+1)+" = ");
-			sarr[i]=ink.readLine();
+		System.out.println("Enter " + num + " numbers to sort::");
+		String sarr[] = new String[num];
+		for (int i = 0; i < num; i++) {
+			System.out.print("No. " + (i + 1) + " = ");
+			sarr[i] = ink.readLine();
 			p.println(sarr[i]);
 			p.flush();
 		}
 		String res;
-		System.out.println("\nSorted array::\n");
-		while((res=in.readLine())!=null)
-		{
+		System.out.println("\nSorted Numbers received from server::");
+		while ((res = in.readLine()) != null) {
 			System.out.println(res);
-		}			
+		}
 		s.close();
-			
+
 	}
 }
--- Socket-Sort/SortServer.java
+++ Socket-Sort/SortServer.java
@@ -1,84 +1,83 @@
+
 // Rohit Raj Anirudh Veludandi
 // Server side code for sorting
+// First run SortServer program
 import java.io.*;
 import java.net.*;
 
 // quicksort 
 class QuickSort {
-	
+
 	int partition(int[] arr, int low, int high) {
 		int p = arr[high];
 		// arr[high] is selected as pivot;
 		int i = low - 1;
-		for(int j = low; j < high; ++j) {
-			if(arr[j] <= p) {
+		for (int j = low; j < high; ++j) {
+			if (arr[j] <= p) {
 				++i;
 				int temp = arr[i];
 				arr[i] = arr[j];
 				arr[j] = temp;
 			}
 		}
-		int temp = arr[i+1];
-		arr[i+1] = arr[high];
+		int temp = arr[i + 1];
+		arr[i + 1] = arr[high];
 		arr[high] = temp;
-		
-		return i+1;
+
+		return i + 1;
 	}
-	
+
 	void quick_sort(int[] arr, int low, int high) {
-		if(low < high) {
+		if (low < high) {
 			// elements less than arr[q] are left side of q
 			// elements greater than arr[q] are right side of q
-			int q = partition(arr, low, high); 
-			quick_sort(arr, low, q-1); // sorts left partition
-			quick_sort(arr, q+1, high); // sorts right partition
+			int q = partition(arr, low, high);
+			quick_sort(arr, low, q - 1); // sorts left partition
+			quick_sort(arr, q + 1, high); // sorts right partition
 		}
 	}
 
 }
 
-class SortServer
-{
-	public static void main(String ar[]) throws Exception
-	{
-		ServerSocket s1=new ServerSocket(12345); // Object of ServerSocket instantiated and port number specified
+class SortServer {
+	public static void main(String ar[]) throws Exception {
+		ServerSocket ss = new ServerSocket(12345); // Object of ServerSocket instantiated and port number specified
 		System.out.println("Server Started");
-		Socket s=s1.accept(); // accpet method of ServerSocket
+		Socket s = ss.accept(); // accpet method of ServerSocket
 
 		// InputStream of client coupling with OutputStream of server and vice versa
-		PrintWriter p=new PrintWriter(s.getOutputStream());
-		BufferedReader in=new BufferedReader(new InputStreamReader(s.getInputStream()));
-		String num=in.readLine();
-		int n=Integer.parseInt(num);
-		System.out.println("Client want to sort "+ n +" numbers");
-		// Receiving  and storing integer inputs to be sorted
-		String sarr[]=new String[n];
-		int arr[]=new int[n];
+		PrintWriter p = new PrintWriter(s.getOutputStream());
+		BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+		String num = in.readLine();
+		int n = Integer.parseInt(num);
+		System.out.println("Client wants to sort " + n + " numbers");
+		// Receiving and storing integer inputs to be sorted
+		String sarr[] = new String[n];
+		int arr[] = new int[n];
 		int c;
 		// To show the numbers in realtime which client is entering
-		System.out.println("Received numbers::\n");
-		for(int i=0;i<n;i++)
-		{
-			sarr[i]=in.readLine();
-			arr[i]=Integer.parseInt(sarr[i]);
-			System.out.println("No. " + (i+1) + " = " + arr[i]);
-		
+		System.out.println("Numbers received from client::\n");
+		for (int i = 0; i < n; i++) {
+			sarr[i] = in.readLine();
+			arr[i] = Integer.parseInt(sarr[i]);
+			System.out.println("No. " + (i + 1) + " = " + arr[i]);
+
 		}
 		// Doing the sorting server side
 		QuickSort obj = new QuickSort();
-		obj.quick_sort(arr, 0, n-1);
-		
+		obj.quick_sort(arr, 0, n - 1);
+
 		// Showing the sorted list of Numbers
-		System.out.println("\nSorted list of numbers");
-		String sendarr=new String();
-		for (c = 0; c < n; c++) 
-		{
-			sendarr+="\nnum ("+c+")="+arr[c];
+		System.out.println("\nSorted list of numbers sent to client::");
+		String sendarr = new String();
+		for (c = 0; c < n; c++) {
+			sendarr += "\nNo. " + (c + 1) + " = " + arr[c];
 		}
 		System.out.println(sendarr);
 		p.println(sendarr);
 		p.flush();
 		s.close();
+		ss.close();
 
 	}
-}  
+}
