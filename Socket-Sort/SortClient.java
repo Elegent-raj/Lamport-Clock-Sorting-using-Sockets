f04842616f022e44a98ce215907419b763dbdd09
Rohit <raj85404@gmail.com>
2021-10-11 20:45:27 +0530

    Final sort sommit

--- Socket-Sort/SortClient.java
+++ Socket-Sort/SortClient.java
@@ -1,13 +1,19 @@
+// Rohit Raj Anirudh Veludandi
+// Client side code for sorting
+// TCP/IP implementation
 import java.io.*;
 import java.net.*;
 class SortClient
 {
 	public static void main(String ar[]) throws Exception
 	{
-		Socket s=new Socket("localhost",12345);
+		// Creating socket and connecting it to server
+		Socket s=new Socket("localhost",12345); 
+		// Serializing the input and output streams
 		PrintWriter p=new PrintWriter(s.getOutputStream());
 		BufferedReader in=new BufferedReader(new InputStreamReader(s.getInputStream()));
 		BufferedReader ink=new BufferedReader(new InputStreamReader(System.in));
+		// Taking the input from client
 		System.out.println("How many numbers to sort? ");
 		int num=Integer.parseInt(ink.readLine());
 		p.println(num);
@@ -16,7 +22,7 @@ class SortClient
 		String sarr[]=new String[num];
 		for(int i=0;i<num;i++)
 		{
-			System.out.print("no. "+i+"=");
+			System.out.print("No. "+(i+1)+" = ");
 			sarr[i]=ink.readLine();
 			p.println(sarr[i]);
 			p.flush();
